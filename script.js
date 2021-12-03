659379b83aad3922a84e4245d0de160a5812cadc
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-12-03 18:53:48 -0300

    requisito 17

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('password');
 const btnLogIn = document.getElementById('btnLogin');
+const checkboxVerifica = document.querySelector('#agreement');
+const btnSubmit = document.getElementById('submit-btn');
 
 function checarLogIn(event) {
   event.preventDefault();
-  console.log('email', inputEmail.value);
+  // console.log('email', inputEmail.value);
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
-    console.log('funcionou login', inputSenha.value);
+    // console.log('funcionou login', inputSenha.value);
     alert('Olá, Tryber!');
   } else {
     window.alert('Email ou senha inválidos.');
@@ -14,3 +16,13 @@ function checarLogIn(event) {
 }
 
 btnLogIn.addEventListener('click', checarLogIn);
+
+btnSubmit.disabled = true;
+console.log('valor do check:', checkboxVerifica);
+
+function verificaCheckAgreement() {
+  if (checkboxVerifica.checked === true) {
+    btnSubmit.disabled = false;
+  }
+}
+checkboxVerifica.addEventListener('click', verificaCheckAgreement);
