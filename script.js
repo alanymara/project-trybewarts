c52fc8a0bb18f6a181b378276f6b995dd48f80ca
alanymara <alanymara@gmail.com>
2021-12-03 22:01:32 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputSenha = document.getElementById('password');
 const btnLogIn = document.getElementById('btnLogin');
 const checkboxVerifica = document.querySelector('#agreement');
 const btnSubmit = document.getElementById('submit-btn');
+const textarea = document.getElementById('textarea');
 
 function checarLogIn(event) {
   event.preventDefault();
@@ -26,3 +27,17 @@ function verificaCheckAgreement() {
   }
 }
 checkboxVerifica.addEventListener('click', verificaCheckAgreement);
+
+// https://pt.stackoverflow.com/questions/25753/como-fazer-um-contador-de-caracteres-de-uma-textarea
+function limiteDeCarac(valor) {
+  const quantMax = 500;
+  const total = valor.length;
+  if (total <= quantMax) {
+    const restante = quantMax - total;
+    document.getElementById('counter').innerHTML = restante;
+  } else {
+    document.getElementById('textarea').value = valor.substr(0, quantMax);
+  }
+}
+
+textarea.addEventListener('click', limiteDeCarac);
