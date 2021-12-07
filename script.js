017bdd539ab76d29def4398853c8913d7c5c7a92
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-12-07 01:53:13 -0300

    requisito 21 + pendencia correcao alinhamento conteudos tecs

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const checkboxVerifica = document.querySelector('#agreement');
 const btnSubmit = document.getElementById('submit-btn');
 const textarea = document.getElementById('textarea');
 
+//  dados do formuario //
+
+const formEvaluation = document.getElementById('evaluation-form');
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const inputEmailForm = document.getElementById('input-email');
+const selectHouse = document.querySelector('#house');
+
+// decalações de funções //
+
 function checarLogIn(event) {
   event.preventDefault();
   // console.log('email', inputEmail.value);
@@ -44,3 +54,24 @@ function limiteDeCarac() {
 }
 
 textarea.addEventListener('keyup', limiteDeCarac);
+
+function substituiFormulario(event) {
+  const familiaTec = document.querySelector('input[name=family]:checked');
+  const tecnologias = document.querySelectorAll('.subject:checked');
+  const notasAvalia = document.querySelector('input[name=rate]:checked');
+  event.preventDefault();
+  let tecs = '';
+  for (let i = 0; i < tecnologias.length; i += 1) {
+    tecs += `${tecnologias[i].value}, `;
+  }
+  formEvaluation.innerText = `
+    Nome: ${inputName.value} ${inputLastName.value}
+    Email: ${inputEmailForm.value} Casa: ${selectHouse.value}
+    Família: ${familiaTec.value} 
+    Matérias: ${tecs} 
+    Avaliação: ${notasAvalia.value}
+    Observações: ${textarea.value}
+  `;
+}
+
+btnSubmit.addEventListener('click', substituiFormulario);
